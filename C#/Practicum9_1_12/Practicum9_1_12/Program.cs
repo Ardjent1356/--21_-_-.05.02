9eed3cecb525fc92d0030c0aff9f6d1f206abb8a
Ardjent1356 <114286679+Ardjent1356@users.noreply.github.com>
2023-03-01 17:14:52 +0400

    Add files via upload

---
+++ C#/Practicum9_1_12/Practicum9_1_12/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Practicum9_1_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длинну от 1 до 8:");
+            int i = int.Parse(Console.ReadLine());
+            using (StreamReader fileIn = new StreamReader(@"C:\Users\horro\OneDrive\Рабочий стол\C#\file.txt"))
+            {
+                string line = fileIn.ReadToEnd();
+                string[] s = line.ToString().Trim().Split(' ', (char)StringSplitOptions.RemoveEmptyEntries);
+                Console.WriteLine("Искомые слова:");
+                foreach (string ln in s)
+                {
+                   
+                    if (ln.Length ==  i)
+                    {
+                        Console.WriteLine(ln);
+                    }
+                   
+                }
+
+                Console.ReadKey();
+                
+               
+            }
+            
+            
+               
+        
+        }
+        
+    }
+}
+
+
---
+++ C#/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Practicum9_1_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длинну:");
+            int i = int.Parse(Console.ReadLine());
+            using (StreamReader fileIn = new StreamReader(@"C:\Users\horro\OneDrive\Рабочий стол\C#\file.txt"))
+            {
+                string line = fileIn.ReadToEnd();
+                StringBuilder a = new StringBuilder(line);
+                string[] s = a.ToString().Trim().Split(' ', (char)StringSplitOptions.RemoveEmptyEntries);
+                Console.WriteLine("Искомые слова:");
+                foreach(string ln in s)
+                {
+                    if (ln.Length==i)
+                    {
+                        Console.WriteLine(ln);
+                    }
+                }
+                
+            }
+            
+            
+               
+        
+        }
+        
+    }
+}
+
+
