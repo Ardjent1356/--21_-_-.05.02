a32462a7b884d6e40efc96966e98972fa9fa1053
Ardjent1356 <114286679+Ardjent1356@users.noreply.github.com>
2023-03-19 16:23:11 +0400

    Add files via upload

---
+++ LaB_5_9/LaB_5_9/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaB_5_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x;
+            double y;
+            for (x=0; x<4; x+=0.25)
+            {
+                if (x < 1)
+                {
+                    y = (2 - x);
+                }
+                else
+                {
+                    y = 2 + (Math.Sqrt(2 * 2 - (x - 2) * (x - 2)));
+                }
+                Console.WriteLine(y);
+            }
+            Console.ReadLine();
+        }
+    }
+}
